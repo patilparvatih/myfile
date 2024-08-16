5f5670a95fa165e33e261e5d265fe30f335c11c3
Mallikarjun <165899099+patilparvatih@users.noreply.github.com>
2024-08-16 15:04:16 +0530

    Add files via upload

--- Task.js
+++ Task.js
@@ -1,9 +1,30 @@
-function Task(props){
+/*
+const Task=(props)=>{
     return(
-      <div>
-        <input type="checkbox" defaultChecked={props.completed}/>
-        <span>{props.text}</span>
-      </div>
+        <div>
+            <input type="checkbox" defaultChecked={props.checked}/>
+            <span>{props.text}</span>
+        </div>
     );
 }
+*/
+function pr(){
+    let a=document.getElementById("se");
+    let v=`your selected language is ${a.value}`;
+    alert(v);
+}
+function Task(){
+    return(
+        <div>
+            <h1>Select Your favorite programing language</h1>
+            <select onMouseLeave={pr} id="se">
+                <option>Python</option>
+                <option>Java</option>
+                <option>C++</option>
+                <option>JavaScript</option>
+            </select>
+        </div>
+    );
+}
+
 export default Task;
